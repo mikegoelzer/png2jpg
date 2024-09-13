dd88a99916048e1a2d19a7c47506869fc53a1bd5
Mike Goelzer <mikegoelzer@goelzer.org>
2024-09-13 16:11:51 -0600

    add --info

--- png2jpg.py
+++ png2jpg.py
@@ -25,10 +25,15 @@ def convert_png_to_jpg(input_file_path, output_file_path):
     out_image.save(output_file_path, "JPEG")
     print(f"saved to {output_file_path}")
 
+def print_image_info(input_file_path):
+    image = Image.open(input_file_path)
+    print(f"{image.format} ({image.mode}): {image.size[0]} x {image.size[1]}")
+
 def main():
     parser = argparse.ArgumentParser(description="convert PNG <-> JPG")
     parser.add_argument("INPUT_FILE", type=str, help="input image file")
     parser.add_argument("OUTPUT_FILE", type=str, nargs='?', help="optional output image file (default is just to swap extension on input)", default=None)
+    parser.add_argument("-i", "--info", action="store_true", help="just prints information about the input image")
     args = parser.parse_args()
 
     # validate args
@@ -56,7 +61,12 @@ def main():
     if output_dir and not os.path.exists(output_dir):
         os.makedirs(output_dir, exist_ok=True)
 
-    # convert image
+    # print info and bail for -i/--info
+    if args.info:
+        print_image_info(args.INPUT_FILE)
+        return 0
+
+    # convert
     convert(args.INPUT_FILE, output_image_path)
 
 if __name__ == "__main__":
